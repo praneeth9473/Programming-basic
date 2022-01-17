1c8ae3065362f897d4da30124b00b0635c20a983
praneeth9473 <87891800+praneeth9473@users.noreply.github.com>
2022-01-17 20:14:16 +0530

    Create generics.cpp

---
+++ generics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/**
+*    Name: printArray
+*    Print each element of the generic vector on a new line. Do not return anything.
+*    @param A generic vector
+**/
+
+// Write your code here
+/**
+…
+}
+int main() {
